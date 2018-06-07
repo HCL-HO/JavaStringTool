3f9ca222df25dbd259014d0f55fbce796eacb813
Eric Ho <eho@ebsl.hk>
2018-06-07 22:52:04 +0800

    Change actioner

--- src/main/java/com/clhoac/java/ReadText.java
+++ src/main/java/com/clhoac/java/ReadText.java
@@ -4,11 +4,8 @@ import java.io.*;
 
 public class ReadText {
 
-    interface Actioner {
-        String actOnLine(String line);
-    }
 
-    public static String readFile(File file, Actioner actioner) {
+    public static String readFile(File file, TextActioner actioner) {
         try {
             Reader reader = new FileReader(file);
             return read(reader, actioner);
@@ -22,12 +19,12 @@ public class ReadText {
         return readFile(file, null);
     }
 
-    public static String readText(String text, Actioner actioner) {
+    public static String readText(String text, TextActioner actioner) {
         Reader reader = new StringReader(text);
         return read(reader, actioner);
     }
 
-    private static String read(Reader reader, Actioner actioner) {
+    private static String read(Reader reader, TextActioner actioner) {
         String result = "";
         try {
             BufferedReader br = new BufferedReader(
